2047f88a93d0eaba5407cfd59afd5495fbc5a108
Lorena <lorena.giddens@outlook.com>
2018-07-16 21:12:49 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, characters.species
+  "SELECT series.title
    FROM series
    INNER JOIN characters
    ON series.id = characters.series_id
