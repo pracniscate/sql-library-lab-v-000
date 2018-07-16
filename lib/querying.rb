1a2ebece010c415adff9a16856a638555ed0a19e
Lorena <lorena.giddens@outlook.com>
2018-07-16 21:07:51 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ def select_value_and_count_of_most_prolific_species
    FROM characters
    GROUP BY characters.species
    ORDER BY COUNT(characters.species)
-   DESC"
+   DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
